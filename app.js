71c940f52993ae8798e2c99af759c08aa4c0df9d
Fernando Nicolás <fersanti2896@gmail.com>
2023-01-23 00:58:41 -0600

    Creando menú con readline en helpers/

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+
 require('colors');
+
+const { mostrarMenu, pause } = require('./helpers/mensajes');
 
 const main = async() => {
     console.clear();
 
     console.log('¡Hola mundo!');
+    let opt = '';
+
+    do {
+        opt = await mostrarMenu(); 
+        console.log({ opt });
+
+        if( opt !== '0' ) await pause();
+    } while (opt !== '0');
 }
 
 main();
